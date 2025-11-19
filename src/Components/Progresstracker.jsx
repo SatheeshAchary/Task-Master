8ec3fc1bc58ad3501c6c402b463ac56c5716c46b
SatheeshAchary <satheeshacharysatti@gmail.com>
2025-11-19 12:46:20 +0530

    19 Nov 2025 commit

--- src/Components/Progresstracker.jsx
+++ src/Components/Progresstracker.jsx
@@ -1,6 +1,20 @@
-function Progresstracker() {
-    return(
-        <h1>This is Progress Tracker</h1>
-    )
+import React from 'react'
+
+export default function ProgressTracker({tasks}) {
+    const completedTasks = tasks.filter((t)=>t.completed).length
+    const totaltasks = tasks.length;
+    const percenteage = totaltasks == 0 ? 0: (completedTasks/totaltasks) * 100;
+  return (
+    <div className='progress-tracker'>
+        <p>
+            {completedTasks} out of {totaltasks} tasks Completed
+        </p>
+
+        <div className='progress-bar'>
+            <div className='progress'
+            style={{width: `${percenteage}%`}}>
+            </div>
+        </div>
+    </div>
+  )
 }
-export default Progresstracker;
--- src/Components/Taskform.jsx
+++ src/Components/Taskform.jsx
@@ -1,26 +1,42 @@
-function Taskform() {
-    return(
-            <form>
-                <div>
-                <input type = "text" placeholder="Enter the Task..."></input>
-                <button type="submit">Add Task</button>
-                </div>
+import React from 'react'
+import { useState } from 'react'
+export default function TaskForm({addTask}) {
+    const [task, setTask] = useState('');
+    const [priority, setPriority] = useState("Medium")
+    const [category, setCategory] = useState("General")
 
-                <div>
-                <select>
-                    <option value="high">High</option>
-                    <option value="medium">Medium</option>
-                    <option value="low">Low</option>
-                </select>
+    const handlesubmit = (e)=>{
+        e.preventDefault();
+        addTask({text:task, priority, category, completed: false})
+        //Reset State:
+        setTask(" ");
+        setPriority("Medium")
+        setCategory('General')
+    }
+  return (
+    <form onSubmit={handlesubmit} className='task-form'>
+        <div id="inp">
+            <input 
+            type = "text"
+            placeholder='Enter Your Task'
+            value={task}
+            onChange={(e)=>setTask(e.target.value)} />
+            <span><button type='submit'>Add Task</button></span>
+        </div>
 
-                <select>
-                    <option value="general">General</option>
-                    <option value="work">Work</option>
-                    <option value="personal">Personal</option>
-                </select>
-                </div>
-                
-            </form>
-    )
+        <div id="btns">
+            <select value={priority} onChange={(e)=>setPriority(e.target.value)}>
+                <option value="High">High</option>
+                <option value="Medium">Medium</option>
+                <option value="Low">Low</option>
+            </select>
+
+            <select value={category} onChange={(e)=>setCategory(e.target.value)}>
+                <option value="General">General</option>
+                <option value="Work">Work</option>
+                <option value="Personal">Personal</option>
+            </select>
+        </div>
+    </form>
+  )
 }
-export default Taskform;
